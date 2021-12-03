ca00f4d0bc84d00fa98761504409a80896356b57
Mine Çetinkaya-Rundel <cetinkaya.mine@gmail.com>
2021-12-03 10:44:38 -0500

    Document animal_sounds

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -1,3 +1,17 @@
+#' Make animal sounds
+#'
+#' Print the given sound of a given animal.
+#'
+#' @param animal A character string of animal name
+#' @param sound A character string of animal sound
+#'
+#' @return A character string with a sentence
+#' @export
+#'
+#' @examples
+#'
+#' animal_sounds(animal = "cat", sound = "meow")
+#'
 animal_sounds <- function(animal, sound) {
   assertthat::assert_that(
     assertthat::is.string(animal),
